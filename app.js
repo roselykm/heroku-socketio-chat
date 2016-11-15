2f41ab1e6fa6f364f80866876cb5b901cf5e2fae
rosely kumoi <roselykm@gmail.com>
2016-11-16 00:31:22 +0800

    A socket.io chat on heroku
    A socket.io chat on heroku

---
+++ app.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+
+  	socket.on('disconnect', function(){
+    	console.log('user disconnected');
+  	});
+
+  	socket.on('message', function(msg){
+    	console.log('Message Received: ', msg);
+    	socket.broadcast.emit('message', msg);
+  	});
+});
+
+http.listen(app.get('port'), function() {
+  console.log('Socket.io char is running on port', app.get('port'));
+});
